b41fcda01168c540c5e013cd7a8df0a8f2dde0e4
jp calvo <calvojp92@gmail.com>
2024-03-25 22:00:28 +0800

    fix

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,6 +4,9 @@
 
 <Counter.Root style="display:flex;gap:0.25rem">
 	{#snippet children(item)}
-		<Counter.Item {item} style="width:2rem;height:2rem;background:black;color:white;" />
+		<Counter.Item
+			{item}
+			style="width:2rem;height:2rem;background:black;color:white;border-radius:0.5em;font-weight:bold;"
+		/>
 	{/snippet}
 </Counter.Root>
