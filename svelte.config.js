922fee58974247a16e13062fe1a71f79b59a754a
jp calvo <calvojp92@gmail.com>
2024-04-21 17:27:26 +0800

    fix

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,13 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-node';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   preprocess: vitePreprocess(),
   kit: {
-    adapter: adapter(),
+    adapter: adapter({
+      out: 'dist',
+    }),
   },
 };
 
