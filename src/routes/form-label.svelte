edf954bee21c5a509270c9661cae1706087243a9
jp calvo <calvojp92@gmail.com>
2024-04-22 18:14:57 +0800

    fix: label color

--- src/routes/form-label.svelte
+++ src/routes/form-label.svelte
@@ -2,14 +2,10 @@
   import { cx } from '$lib/cx.js';
   import type { SvelteHTMLElements } from 'svelte/elements';
 
-  let {
-    class: className,
-    children,
-    ...props
-  }: SvelteHTMLElements['label'] = $props();
+  let { class: className, children, ...props }: SvelteHTMLElements['label'] = $props();
 </script>
 
-<label for="" class={cx('mb-1 text-xs block', className)} {...props}>
+<label for="" class={cx('mb-1 text-xs text-gray-500 block', className)} {...props}>
   {#if children}
     {@render children()}
   {/if}
